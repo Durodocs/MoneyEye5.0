43a28305634e9c2d64f1f069a4b6fb50a9916219
Durodocs <alexisgokuelpro02@gmail.com>
2025-10-06 00:06:27 -0600

    build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,14 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
 }
+
+plugins {
+  id("org.sonarqube") version "6.3.1.5724"
+}
+
+sonar {
+  properties {
+    property("sonar.projectKey", "Durodocs_MoneyEye5.0_a0941395-83ff-469d-8ee0-f442d9d41add")
+    property("sonar.projectName", "MoneyEye5.0")
+  }
+}
